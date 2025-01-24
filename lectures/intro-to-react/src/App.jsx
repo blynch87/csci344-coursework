a879f06d4d0355bb8593aa22fbe8f97f10b641ee
Ben <blynch@unca.edu>
2025-01-24 13:29:35 -0500

    My First Commit... 2

--- lectures/intro-to-react/src/App.jsx
+++ lectures/intro-to-react/src/App.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Profile from "./Profile";
+import ButtonCount from "./ButtonCount.jsx";
+import { getPosts } from "./data-functions.js";
+import "./App.css";
 
 export default function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    (async function () {
+        const posts = await getPosts();
+        console.log(posts);
+        setPosts(posts);
+    })();
+}, []);
+
+  function generateProfileComponentsFromData() {
+    return posts.map((item) => (
+        <Profile name={item.user.first_name} picture={item.user.image_url} bio={item.caption}/>
+    ));
+  }
+
   return (
     <>
       <header>
@@ -9,11 +28,46 @@ export default function App() {
       </header>
       <main>
         <p>Hello react!</p>
-        <Profile name="Anita" year="2005" school="dwyer"/>
-        <Profile name="Ben" />
-        <Profile name="Adwaina" />
-        <Profile name="Laciesha" />
+        <div className="people">
+           {generateProfileComponentsFromData()}
+        </div>
+
+
+      <ButtonCount intitialValue={2}/>
+      <ButtonCount intitialValue={4} />
+      <ButtonCount intitialValue={6} />
+      <ButtonCount intitialValue={8} />
+      <ButtonCount intitialValue={10} />
       </main>
     </>
   );
 }
+
+// const people = [
+//   {
+//     name: "Anita",
+//     image_url: "https://picsum.photos/id/216/100/100",
+//     year: "2020" ,
+//     school: "Asheville High" ,
+//   },
+//   {
+//     name: "Ben",
+//     image_url: "https://picsum.photos/id/217/100/100",
+//     year: "2005" ,
+//     school: "Santaluces" ,
+//   },
+//   {
+//     name: "Adwaina",
+//     image_url: "https://picsum.photos/id/218/100/100",
+//     year: "1995" ,
+//     school: "Lake Worth High" ,
+//   },
+//   {
+//     name: "Laciesha",
+//     image_url: "https://picsum.photos/id/219/100/100",
+//     year: "1985" ,
+//     school: "Erwin High" ,
+//   },
+// ];
+
+
--- lectures/intro-to-react/src/Profile.jsx
+++ lectures/intro-to-react/src/Profile.jsx
@@ -1,13 +1,16 @@
 import "./Profile.css";
 import React from "react";
 
- export default function Profile({name, year, school}) {
+ export default function Profile({name, picture, year, school}) {
         
      return (
          <section className="profile">
-             <h3>{name}</h3>
-             <p>{year}</p>
-             <p>{school}</p>
+             <h2>{name}</h2>
+             <img src={picture} />
+             <p>{bio}</p>
+
+
+
          </section>
      );
  }
